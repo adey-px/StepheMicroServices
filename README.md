7deb76a55b8b7b1a5f6779368b810c42eea78659
adey-px <aosteph@gmail.com>
2023-07-08 23:25:14 -0500

    Configured express backend API service

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Stephe Microservices
-* This is a Node.js project based on building application using microservices architecture approach.
+* This is a Node.js project based on building application using microservices architecture approach
+- it comprises of three features referred to as services namely client, posts and comments
+- each feature is built and managed as an independent service or microservice from other services
+- each feature or service has its own server, business logic and standalone database
---
+++ posts/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { randomBytes } from 'crypto';
+
+// Instance of server
+const app = express();
+app.use(bodyParser.json());
+
+// Empty object to store posts
+const posts = {};
+
+// API to get all posts
+app.get('/posts', (req, res) => {
+	res.send(posts);
+});
+
+// API to create new posts
+app.post('/posts', (req, res) => {
+	const id = randomBytes(4).toString('hex');
+	const { title } = req.body;
+
+	posts[id] = {
+		id,
+		title,
+	};
+	res.status(201).send(posts[id]);
+});
+
+// Configure server
+app.listen(4000, () => {
+	console.log('Started development server');
+});
