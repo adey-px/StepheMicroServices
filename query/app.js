37c19ab78a3376dcdf2cceed40dd41b761fc6d2d
adey-px <aosteph@gmail.com>
2023-08-20 18:40:15 -0500

    Setup Query API service

---
+++ query/app.js
@@ -0,0 +1,17 @@
+import express, { json } from 'express';
+import cors from 'cors';
+
+//
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+app.get('/posts', (req, res) => {});
+
+// API - receive event from event bus
+app.post('events/', (req, res) => {});
+
+app.listen(5003, () => {
+	console.log('**Started Query api service');
+});
